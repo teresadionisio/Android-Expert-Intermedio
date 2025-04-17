592a0557904b1b48f40d400b9737194e3951df50
teresadionisio <teresa.dionisio@gmail.com>
2025-04-16 22:23:40 -0500

    Proyecto inicial

---
+++ app/src/main/java/com/aristidevs/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.aristidevs.horoscapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.aristidevs.horoscapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding //Para declarar accesos a xml y pantallas
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =
+            ActivityMainBinding.inflate(layoutInflater) //Para declarar accesos a xml y pantallas
+        setContentView(binding.root)//Para declarar accesos a xml y pantallas
+        initUI()
+    }
+
+    /**************************************************************************/
+    private fun initUI() {
+        initNavigation()
+    }
+
+    /**************************************************************************/
+    private fun initNavigation() {
+        val navHost: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+    /**************************************************************************/
+}
